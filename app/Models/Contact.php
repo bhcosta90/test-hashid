a9a57f312f4a47cd67ec147c009aab20f15becea
Bruno Henrique da Costa <bruno.costa@mobiis.com.br>
2025-03-18 20:21:41 -0300

    feat: add Hashids encoding and decoding for contact and customer IDs

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -6,6 +6,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\{Factories\HasFactory, Model, SoftDeletes};
+use Vinkla\Hashids\Facades\Hashids;
 
 class Contact extends Model
 {
@@ -20,5 +21,25 @@ class Contact extends Model
     public function customer(): BelongsTo
     {
         return $this->belongsTo(Customer::class);
+    }
+
+    public function getIdAttribute($value): string
+    {
+        return Hashids::encode($value);
+    }
+
+    public function setIdAttribute($value): void
+    {
+        $this->attributes['id'] = Hashids::decode($value)[0] ?? null;
+    }
+
+    public function getCustomerIdAttribute($value): string
+    {
+        return Hashids::encode($value);
+    }
+
+    public function setCustomerIdAttribute($value): void
+    {
+        $this->attributes['customer_id'] = Hashids::decode($value)[0] ?? null;
     }
 }
